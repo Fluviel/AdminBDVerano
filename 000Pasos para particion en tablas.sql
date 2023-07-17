01485f9f372d161483d985296f640749b9a46628
Fluviel <138792332+Fluviel@users.noreply.github.com>
2023-07-17 08:23:06 -0600

    Update 000Pasos para particion en tablas.sql

--- 000Pasos para particion en tablas.sql
+++ 000Pasos para particion en tablas.sql
@@ -21,6 +21,22 @@ CREATE TABLE product_partition_2 PARTITION OF product
 
 CREATE TABLE product_partition_3 PARTITION OF product
     FOR VALUES FROM (6000.01) TO (10000.00);
+
+-- Para la partición product_partition_1-2-3
+ALTER TABLE product_partition_1
+ADD CONSTRAINT product_partition_1_price_check
+CHECK (price >= 1.00 AND price <= 3000.00);
+
+
+ALTER TABLE product_partition_2
+ADD CONSTRAINT product_partition_2_price_check
+CHECK (price >= 3000.01 AND price <= 6000.00);
+
+
+ALTER TABLE product_partition_3
+ADD CONSTRAINT product_partition_3_price_check
+CHECK (price >= 6000.01 AND price <= 10000.00);
+
 --Importar los registros a nuestra tabla--
 \i /var/lib/postgresql/product.sql
 --Listamos las tablas creadas--
@@ -56,6 +72,10 @@ CREATE TABLE client_partition_female PARTITION OF client
 
 CREATE TABLE client_partition_male PARTITION OF client
     FOR VALUES IN ('Male', 'male', 'MALE');
+
+ALTER TABLE client_partition_female ADD CONSTRAINT partition_female_check CHECK (gender IN ('Female', 'female', 'FEMALE'));
+
+ALTER TABLE client_partition_male ADD CONSTRAINT partition_male_check CHECK (gender IN ('Male', 'male', 'MALE'));
 
 --Importar los registros a nuestra tabla--
 \i /var/lib/postgresql/client.sql
